20bd7d32ec466768252ec0ee7a42a20ff98af957
Ilia <ilytar@gmail.com>
2022-08-19 18:05:22 +0300

    Начинает передачу данных во вью

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -2,6 +2,9 @@ import {render} from '../render.js';
 import tripDestinationPointView from './../view/destination-point.js';
 import ContentContainerView from './../view/content-container-view.js';
 
+import TripDestinationView from './../view/destination-view.js';
+
+
 export default class BoardPresenter {
   fieldComponent = new ContentContainerView();
 
@@ -9,6 +12,9 @@ export default class BoardPresenter {
     this.fieldContainer = fieldContainer;
     this.destinationCitiesModel = destinationCitiesModel;
     this.destinationCities = [...this.destinationCitiesModel.getCities()];
+
+    render(new TripDestinationView(this.destinationCities), this.fieldComponent.getElement());
+    console.log(this.destinationCities);
 
     for (let i = 0; i < 3; i++) {
       render(this.fieldComponent, this.fieldContainer);
--- src/view/destination-view.js
+++ src/view/destination-view.js
@@ -1,27 +1,34 @@
 import {createElement} from '../render.js';
 
-const createDestinationTemplate = () => (
-  `<section class="event__details">
-    <section class="event__section  event__section--destination">
-      <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-      <p class="event__destination-description">Geneva is a city in Switzerland that lies at the southern tip of expansive Lac Léman (Lake Geneva). Surrounded by the Alps and Jura mountains, the city has views of dramatic Mont Blanc.</p>
+const createDestinationTemplate = () => {
+  // const {description, name} = destination;
+  return (
+    `<section class="event__details">
+      <section class="event__section  event__section--destination">
+        <h3 class="event__section-title  event__section-title--destination">Destination</h3>
+        <p class="event__destination-description">123</p>
 
-      <div class="event__photos-container">
-        <div class="event__photos-tape">
-          <img class="event__photo" src="img/photos/1.jpg" alt="Event photo">
-          <img class="event__photo" src="img/photos/2.jpg" alt="Event photo">
-          <img class="event__photo" src="img/photos/3.jpg" alt="Event photo">
-          <img class="event__photo" src="img/photos/4.jpg" alt="Event photo">
-          <img class="event__photo" src="img/photos/5.jpg" alt="Event photo">
+        <div class="event__photos-container">
+          <div class="event__photos-tape">
+            <img class="event__photo" src="img/photos/1.jpg" alt="Event photo">
+            <img class="event__photo" src="img/photos/2.jpg" alt="Event photo">
+            <img class="event__photo" src="img/photos/3.jpg" alt="Event photo">
+            <img class="event__photo" src="img/photos/4.jpg" alt="Event photo">
+            <img class="event__photo" src="img/photos/5.jpg" alt="Event photo">
+          </div>
         </div>
-      </div>
-    </section>
-  </section>`
-);
+      </section>
+    </section>`
+  );
+};
 
 export default class TripDestinationView {
+  constructor (destination) {
+    this.destination = destination;
+  }
+
   getTemplate() {
-    return createDestinationTemplate();
+    return createDestinationTemplate(this.destination);
   }
 
   getElement() {
