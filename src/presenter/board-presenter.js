5831554e571e7448aa15b9e8fa8889ec6da51a48
Ilia <ilytar@gmail.com>
2022-08-23 22:20:26 +0300

    Переделывает структуру файлов

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,12 +1,17 @@
 import {render} from '../render.js';
 import tripDestinationPointView from '../view/destination-point-view.js';
 import ContentContainerView from './../view/content-container-view.js';
-
+import WrapperContentContainerView from './../view/wrapper-content-container-view.js';
+import WrapperFormContentContainerView from './../view/wrapper-form-content-container-view.js';
 import TripDestinationView from './../view/destination-view.js';
+import TripAddOptionsView from './../view/trip-offers-view.js';
+import TripParametersView from './../view/new-trip-parameters-view.js';
 
 
-export default class BoardPresenter {
-  #fieldComponent = new ContentContainerView();
+export default class AppPresenter {
+  #tripListComponent = new ContentContainerView();
+  #tripItemComponent = new WrapperContentContainerView();
+  #tripItemFormComponent = new WrapperFormContentContainerView();
 
   #fieldContainer = null;
   #destinationCitiesModel = null;
@@ -22,11 +27,16 @@ export default class BoardPresenter {
     this.#transportTypeModel = transportTypeModel;
     this.#transportType = [...this.#transportTypeModel.transportType];
 
-    render(new TripDestinationView(this.#destinationCities[0]), this.#fieldComponent.element);
+    render(this.#tripListComponent, this.#fieldContainer);
+    render(this.#tripItemComponent, this.#tripListComponent.element);
+    render(this.#tripItemFormComponent, this.#tripItemComponent.element);
+    render(new TripParametersView(), this.#tripItemFormComponent.element);
+    render(new TripAddOptionsView(), this.#tripItemFormComponent.element);
+    render(new TripDestinationView(this.#destinationCities[0]), this.#tripItemFormComponent.element);
 
     for (let i = 0; i < 3; i++) {
-      render(this.#fieldComponent, this.#fieldContainer);
-      render(new tripDestinationPointView(this.#transportType[0]), this.#fieldComponent.element);
+      render(this.#tripListComponent, this.#fieldContainer);
+      render(new tripDestinationPointView(this.#transportType[0]), this.#tripListComponent.element);
     }
   };
 }
--- src/view/wrapper-form-content-container-view.js
+++ src/view/wrapper-form-content-container-view.js
@@ -1,16 +1,16 @@
 import {createElement} from '../render.js';
 
-const createContentContainerTemplate = () => (
-  `<ul class="trip-events__list">
-  </ul>`
+const createWrapperFormContentContainerTemplate = () => (
+  `<form class="event event--edit" action="#" method="post">
+  </form>`
 );
 
 
-export default class ContentContainerView {
+export default class WrapperFormContentContainerView {
   #element = null;
 
   get template() {
-    return createContentContainerTemplate();
+    return createWrapperFormContentContainerTemplate();
   }
 
   get element() {
