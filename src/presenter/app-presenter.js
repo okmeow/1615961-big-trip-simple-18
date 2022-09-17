cf65d60de8c09a1f3bee81fad09659f81953f51d
Ilia <ilytar@gmail.com>
2022-09-17 19:28:54 +0300

    Подготовит рендер-функции к модели

--- src/presenter/app-presenter.js
+++ src/presenter/app-presenter.js
@@ -6,7 +6,7 @@ import EmptyPointListMessageView from '../view/empty-point-list-message-view.js'
 import SortView from '../view/sort-view.js';
 import ButtonNewEventView from '../view/button-new-event-view.js';
 import PointPresenter from './point-presenter.js';
-import {updateArrayElement, sortPointDateUp, sortPointPriceDown, isEscapeKey} from '../utils/utils.js';
+import {sortPointDateUp, sortPointPriceDown, isEscapeKey} from '../utils/utils.js';
 import {SortType} from '../mock/const.js';
 
 export default class AppPresenter {
@@ -22,16 +22,8 @@ export default class AppPresenter {
   #tripOffersModel = null;
   #tripNewPointCreateComponent = null;
 
-  #tripCities = [];
-  #tripPoints = [];
-  #tripOffers = [];
   #pointPresenter = new Map();
-  #currentSortType = SortType.DEFAULT;
-  #sourcedTripPoints = [];
-
-  #point = null;
-  #offers = [];
-  #cities = [];
+  #currentSortType = SortType.DATE;
 
   constructor (fieldContainer, destinationCitiesModel, tripPointsModel, tripOffersModel) {
     this.#fieldContainer = fieldContainer;
@@ -41,6 +33,13 @@ export default class AppPresenter {
   }
 
   get points() {
+    switch (this.#currentSortType) {
+      case SortType.DATE:
+        return [...this.#tripPointsModel.tripPoints].sort(sortPointDateUp);
+      case SortType.PRICE:
+        return [...this.#tripPointsModel.tripPoints].sort(sortPointPriceDown);
+    }
+
     return this.#tripPointsModel.tripPoints;
   }
 
@@ -53,18 +52,7 @@ export default class AppPresenter {
   }
 
   init = () => {
-    // this.#point = point;
-    // this.#offers = offers;
-    // this.#cities = cities;
-
-    // this.#cities = [...this.#destinationCitiesModel.tripCities];
-    // this.#tripPoints = [...this.#tripPointsModel.tripPoints];
-    // this.#offers = [...this.#tripOffersModel.tripOffers];
-
-    this.#sourcedTripPoints = [...this.#tripPoints.sort(sortPointDateUp)];
-
     this.#tripNewPointCreateComponent = new TripDestinationPointCreateView(this.points[0], this.cities[0], this.offers);
-
     this.#newEventButtonComponent.setNewEventButtonClickHandler(this.#handleNewEventClick);
 
     this.#renderContent();
@@ -84,7 +72,7 @@ export default class AppPresenter {
   #escapeKeyDownHandler = (evt) => {
     if (isEscapeKey(evt)) {
       evt.preventDefault();
-      this.#tripNewPointCreateComponent.reset(this.#point);
+      this.#tripNewPointCreateComponent.reset(this.points);
       this.#deleteNewEventForm();
     }
   };
@@ -102,27 +90,12 @@ export default class AppPresenter {
     render(this.#tripItemComponent, this.#tripContentContainerListComponent.element);
   };
 
-  #sortPoints = (sortType) => {
-    switch (sortType) {
-      case SortType.DATE:
-        this.#tripPoints.sort(sortPointDateUp);
-        break;
-      case SortType.PRICE:
-        this.#tripPoints.sort(sortPointPriceDown);
-        break;
-      default:
-        this.#tripPoints = [...this.#sourcedTripPoints];
-    }
-
-    this.#currentSortType = sortType;
-  };
-
   #handleSortTypeChange = (sortType) => {
     if (this.#currentSortType === sortType) {
       return;
     }
 
-    this.#sortPoints(sortType);
+    this.#currentSortType = sortType;
     this.#clearPointList();
     this.#renderPointList();
   };
@@ -136,10 +109,9 @@ export default class AppPresenter {
     this.#pointPresenter.forEach((presenter) => presenter.resetView());
   };
 
-  #handlePointChange = (updatedPoint) => {
-    this.#tripPoints = updateArrayElement(this.#tripPoints, updatedPoint);
-    this.#sourcedTripPoints = updateArrayElement(this.#sourcedTripPoints, updatedPoint);
-    this.#pointPresenter.get(updatedPoint.id).init(updatedPoint, this.#offers, this.#cities);
+  #handlePointChange = (updatedPoint, offers, cities) => {
+    // this.points = updateArrayElement(this.points, updatedPoint);
+    this.#pointPresenter.get(updatedPoint.id).init(updatedPoint, offers, cities);
   };
 
   #renderNewPointForm = () => {
