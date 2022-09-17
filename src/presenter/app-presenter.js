d506b2ebec5aef908998eb8ca3365ca7525a21c6
Ilia <ilytar@gmail.com>
2022-09-17 20:49:26 +0300

    Замкнет датабиндинг

--- src/presenter/app-presenter.js
+++ src/presenter/app-presenter.js
@@ -7,7 +7,7 @@ import SortView from '../view/sort-view.js';
 import ButtonNewEventView from '../view/button-new-event-view.js';
 import PointPresenter from './point-presenter.js';
 import {sortPointDateUp, sortPointPriceDown, isEscapeKey} from '../utils/utils.js';
-import {SortType} from '../mock/const.js';
+import {SortType, UpdateType, UserAction} from '../mock/const.js';
 
 export default class AppPresenter {
   #tripContentContainerListComponent = new ContentContainerListView();
@@ -61,19 +61,33 @@ export default class AppPresenter {
   };
 
   #handleViewAction = (actionType, updateType, update) => {
-    console.log(actionType, updateType, update);
-    // Здесь будем вызывать обновление модели.
-    // actionType - действие пользователя, нужно чтобы понять, какой метод модели вызвать
-    // updateType - тип изменений, нужно чтобы понять, что после нужно обновить
-    // update - обновленные данные
+    switch (actionType) {
+      case UserAction.UPDATE_POINT:
+        this.#tripPointsModel.updatePoint(updateType, update);
+        break;
+      case UserAction.ADD_POINT:
+        this.#tripPointsModel.addPoint(updateType, update);
+        break;
+      case UserAction.DELETE_POINT:
+        this.#tripPointsModel.deletePoint(updateType, update);
+        break;
+    }
   };
 
   #handleModelEvent = (updateType, data) => {
-    console.log(updateType, data);
-    // В зависимости от типа изменений решаем, что делать:
-    // - обновить часть списка (например, когда поменялось описание)
-    // - обновить список (например, когда задача ушла в архив)
-    // - обновить всю доску (например, при переключении фильтра)
+
+    switch (updateType) {
+      case UpdateType.PATCH:
+        // - обновить часть списка (например, когда поменялось описание)
+        this.#pointPresenter.get(data.id).init(data);
+        break;
+      case UpdateType.MINOR:
+        // - обновить список (например, когда задача ушла в архив)
+        break;
+      case UpdateType.MAJOR:
+        // - обновить всю доску (например, при переключении фильтра)
+        break;
+    }
   };
 
   #handleNewEventClick = () => {
